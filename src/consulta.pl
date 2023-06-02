129c3a53c5f6a64fb834a5850e53ce5ef4aa9e12
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-06-02 01:36:37 -0300

    Adiciona formatacao e estilo na interface do sistema

--- src/consulta.pl
+++ src/consulta.pl
@@ -1,17 +1,42 @@
+:- use_module(library(ansi_term)).
 :- consult('sintomas.pl').
 :- consult('doencas.pl').
 
+clear_screen :-
+    write('\e[H\e[2J').
+
+print_divider :-
+    writeln(''),
+    writeln('--------------------------------------------------------------------------------').
+
+print_title :-
+    print_divider,
+    writeln(''),
+    ansi_format([bold, fg(blue)], '                        Sistema de Diagnóstico Médico ', []),
+    writeln(''),
+    print_divider,
+    writeln('').
+
 consultar_sintomas :-
-    write('Responda "s" para sim e "n" para nao:'), nl,
-    findall(Sintoma, sintoma(Sintoma), Sintomas),   % Obtém a lista de todos os sintomas
-    diagnostico_sintomas(Sintomas, [], Doenca),     % Realiza o diagnóstico com base nos sintomas
-    write('Doenca mais provavel: '), write(Doenca), nl.
+    clear_screen,
+    print_title,
+    writeln('                        Responda "s" para sim e "n" para nao: '),
+    print_divider,
+    writeln(''),
+    findall(Sintoma, sintoma(Sintoma), Sintomas),
+    diagnostico_sintomas(Sintomas, [], Doenca),
+    print_divider,
+    writeln(''),
+    ansi_format([bold, fg(green)], '                        Diagnóstico:', []),
+    ansi_format([bold, fg(yellow)], Doenca, []),
+    writeln(''),
+    print_divider.
 
 diagnostico_sintomas([], _, Doenca) :-
     diagnostico(Doenca).
 
 diagnostico_sintomas([Sintoma|Resto], RespostasAnteriores, Doenca) :-
-    perguntar_sintoma(Sintoma, Resposta),           % Pergunta ao usuário sobre o sintoma atual
+    perguntar_sintoma(Sintoma, Resposta),
     (Resposta = s ->
         append(RespostasAnteriores, [Sintoma], NovasRespostas),
         diagnostico_sintomas(Resto, NovasRespostas, Doenca)
@@ -20,7 +45,7 @@ diagnostico_sintomas([Sintoma|Resto], RespostasAnteriores, Doenca) :-
     ).
 
 perguntar_sintoma(Sintoma, Resposta) :-
-    write('Voce apresenta o sintoma "'), write(Sintoma), write('"? (s/n)'), nl,
+    format('Voce apresenta o sintoma "~w"? (s/n) ', [Sintoma]),
     read(Resposta).
 
 :- initialization(consultar_sintomas).

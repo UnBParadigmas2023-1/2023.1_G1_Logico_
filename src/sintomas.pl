26d8947e63fc2f0f1d8a224969a66e82ab71675f
Gustavo Duarte Moreira <32913216+gustavoduartemoreira@users.noreply.github.com>
2023-06-02 02:04:51 -0300

    Update sintomas.pl

--- src/sintomas.pl
+++ src/sintomas.pl
@@ -1,4 +1,3 @@
-% Sintomas da Febre Amarela
 sintoma(febre_alta).
 sintoma(dor_de_cabeca).
 sintoma(dor_muscular).
@@ -6,41 +5,15 @@ sintoma(fadiga).
 sintoma(nausea).
 sintoma(vomito).
 sintoma(ictericia).
-
-% Sintomas da Dengue
-sintoma(febre_alta).
-sintoma(dor_de_cabeca).
-sintoma(dor_muscular).
-sintoma(fadiga).
-sintoma(nausea).
-sintoma(vomito).
-
-% Sintomas da Gripe
-sintoma(febre_alta).
-sintoma(dor_de_cabeca).
-sintoma(dor_muscular).
-sintoma(fadiga).
-sintoma(nausea).
-
-% Sintomas da Sinusite
 sintoma(dor_rosto).
 sintoma(congestao_nasal).
-sintoma(dor_cabeca).
 sintoma(secrecao_nasal).
 sintoma(dor_garganta).
 sintoma(tosse).
-sintoma(febre_leve).
-
-% Sintomas da Pneumonia
-sintoma(febre_alta).
-sintoma(tosse).
 sintoma(falta_ar).
 sintoma(dor_peito).
-sintoma(fadiga).
 sintoma(confusao_mental).
 sintoma(expectoracao).
-
-% Sintomas da Faringite
 sintoma(dor_garganta).
 sintoma(dificuldade_engolir).
 sintoma(dor_ouvido).
